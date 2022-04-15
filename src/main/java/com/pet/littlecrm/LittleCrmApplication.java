068ea5a40229efdc28a9b8e0d50daf96dd637e8d
Zoya Nazarova <zoyavishni@gmail.com>
2022-04-15 23:59:17 +0700

    initial commit

---
+++ src/main/java/com/pet/littlecrm/LittleCrmApplication.java
@@ -0,0 +1,13 @@
+package com.pet.littlecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pet/littlecrm/LittleCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.littlecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
