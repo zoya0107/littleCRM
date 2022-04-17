de5c4bd6c55b1dd450e13aca4593aabdfc6cff63
Zoya Nazarova <zoyavishni@gmail.com>
2022-04-17 16:48:49 +0700

    model: added model, controller, service, repo

---
+++ src/main/java/com/pet/littlecrm/repository/PersonRepository.java
@@ -0,0 +1,16 @@
+package com.pet.littlecrm.repository;
+
+import com.pet.littlecrm.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    //SELECT * FROM user WHERE login = ?
+    @Query("SELECT u FROM Person u WHERE u.login = ?1")
+    Optional<Person> findUserByEmail(String login);
+}
