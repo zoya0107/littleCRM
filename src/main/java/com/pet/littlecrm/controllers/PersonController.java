69baa387d6e7d66690959e77a0b29f9e1314564e
Zoya Nazarova <zoyavishni@gmail.com>
2022-04-23 17:23:09 +0700

    feature: added dropdowns, person update and delete

--- src/main/java/com/pet/littlecrm/controllers/PersonController.java
+++ src/main/java/com/pet/littlecrm/controllers/PersonController.java
@@ -2,6 +2,8 @@ package com.pet.littlecrm.controllers;
 
 import com.pet.littlecrm.model.Message;
 import com.pet.littlecrm.model.Person;
+import com.pet.littlecrm.model.Role;
+import com.pet.littlecrm.model.Status;
 import com.pet.littlecrm.service.MessageService;
 import com.pet.littlecrm.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping(path="/home")
@@ -23,24 +29,28 @@ public class PersonController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('people:read')")
     public String getHomePage(Model model) {
         model.addAttribute("listMessages", messageService.getMessages());
         return "home";
     }
 
     @GetMapping("/createmessageform")
+    @PreAuthorize("hasAuthority('people:read')")
     public String getNewMessagePage(Model model) {
         model.addAttribute("message", new Message());
         return "newmessage";
     }
 
     @PostMapping("/createmessage")
+    @PreAuthorize("hasAuthority('people:read')")
     public String createNewMessage(@ModelAttribute("message") Message message) {
         messageService.saveMessage(message);
         return "redirect:/home";
     }
 
     @GetMapping("/showmessage/{id}")
+    @PreAuthorize("hasAuthority('people:read')")
     public String showMessage(@PathVariable (value="id") Long id, Model model) {
         Message message = messageService.getMessageById(id);
         model.addAttribute("message", message);
@@ -48,21 +58,46 @@ public class PersonController {
     }
 
     @GetMapping("/createpersonform")
+    @PreAuthorize("hasAuthority('people:write')")
     public String getNewPersonPage(Model model) {
         model.addAttribute("person", new Person());
+        List<Role> roleOptions = new ArrayList<Role>(Arrays.asList(Role.values()));
+        model.addAttribute("roleoptions", roleOptions);
+        List<Status> statusOptions = new ArrayList<Status>(Arrays.asList(Status.values()));
+        model.addAttribute("statusoptions", statusOptions);
         return "add";
     }
 
     @PostMapping("/add")
+    @PreAuthorize("hasAuthority('people:write')")
     public String addPerson(@ModelAttribute("person") Person person) {
         personService.savePerson(person);
-        return "redirect:/home";
+        return "redirect:/home/showlist";
     }
 
-//    @GetMapping("/{id}")
-//    @PreAuthorize("hasAuthority('people:read')")
-//    public Person getById(@PathVariable Long id) {
-//        return personService.getPersonById(id);
-//    }
+    @GetMapping("/showlist")
+    @PreAuthorize("hasAuthority('people:write')")
+    public String getListPeople(Model model) {
+        model.addAttribute("listPeople", personService.getPeople());
+        return "listpeople";
+    }
 
+    @GetMapping("/update/{id}")
+    @PreAuthorize("hasAuthority('people:write')")
+    public String showUpdateForm(@PathVariable (value="id") Long id, Model model) {
+        Person person = personService.getPersonById(id);
+        model.addAttribute("person", person);
+        List<Role> roleOptions = new ArrayList<Role>(Arrays.asList(Role.values()));
+        model.addAttribute("roleoptions", roleOptions);
+        List<Status> statusOptions = new ArrayList<Status>(Arrays.asList(Status.values()));
+        model.addAttribute("statusoptions", statusOptions);
+        return "update";
+    }
+
+    @GetMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('people:write')")
+    public String deletePerson(@PathVariable (value="id") Long id, Model model) {
+        personService.deletePersonById(id);
+        return "redirect:/home/showlist";
+    }
 }
