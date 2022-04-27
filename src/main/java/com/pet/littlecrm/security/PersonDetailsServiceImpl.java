884cdbf2dbd6ac5ce683951bd79f1d6668296e66
Zoya Nazarova <zoyavishni@gmail.com>
2022-04-28 01:16:28 +0700

    fix: changed the way of obtaining current person info

--- src/main/java/com/pet/littlecrm/security/PersonDetailsServiceImpl.java
+++ src/main/java/com/pet/littlecrm/security/PersonDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.pet.littlecrm.security;
 import com.pet.littlecrm.model.Person;
 import com.pet.littlecrm.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -22,5 +23,16 @@ public class PersonDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         Person person = personRepository.findPeopleByLogin(login).orElseThrow(() -> new UsernameNotFoundException("User doesn't exist"));
         return SecurityPerson.fromPerson(person);
+    }
+
+    public String getCurrentPerson() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        return username;
     }
 }
